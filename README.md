6602c87bc2ed2627e27b2014b908c84706c00e10
rahulsai1999 <noneuser2183@gmail.com>
2019-10-24 18:28:02 +0530

    till flow control

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Golang Quickstart
 
 A quickstart for Go-lang with all the necessary code and comments
+
+1. Variables
+2. Functions (Arguments, Signatures and Named returns)
+3. Simple Datatypes
+4. Complex Datatypes
+5. File Handling
+6. Flow Control (Conditionals)
--- everything.go
+++ everything.go
@@ -1,72 +1,108 @@
 package main
 
-import(
+import (
 	"fmt"
 	"os"
 )
 
-func main(){
+func main() {
 	fmt.Println("Hello World")
-	nextHello()
+	aVariables()
+	cTypes()
+	dComplexTypes()
 }
 
-func nextHello(){
-	var x int //variable declaration
-	x=3
-	y:=4 //short declration to infer the type, declare and assign
-	sum,prod:=learnMultiple(x,y)
-	fmt.Println("sum: ",sum,"prod: ",prod)
-	learnTypes()
+func aVariables() {
+	var x int // declaration
+	x = 3     // assignment
+	y := 4    // short declaration - infers the type,declare and assign
+
+	sum, prod := bFunctions(x, y)
+	fmt.Println("Sum:", sum, " Prod:", prod)
 }
 
-// functions with parameters must also contain the return types 
-// func <name>(input params)(return params)
-func learnMultiple(x,y int)(sum,prod int){
-	return x+y,x*y
+func bFunctions(x, y int) (sum, prod int) {
+	// function parameters are passed like this
+	// x, y are the input arguments of type int
+	// sum, prod are the output signatures of type int
+	return x + y, x * y
 }
 
-// some extra variable types
-func learnTypes(){
-	str:="Learn Go!" //string 
-	s2:=`Raw String
-	with breaks and spaces` //raw string
+func bFunctionsNamedReturn(x, y int) (z int) {
+	z = x * y
+	return // z is implicit as only single parameter is present
+}
 
-	g := 'Σ' //UTF-8 source code
-	f:=3.14195 //float
-	c:=3+4i
+func cTypes() {
+	// datatypes available in Go
+	str := "Learn Go!"
+	str2 := `A raw string can include
+	line breaks.`
+	g := 'Σ'
+	f := 3.14195
+	c := 3 + 4i
 
-	var u uint=7 //unsigned int
-	var pi float32=22./7
+	var u uint = 7
+	var pi float32 = 22. / 7
 
-	n:=byte('\n') //byte is also uint8
+	n := byte('\n')
 
-	var a4 [4] int // array declaration
-	a5:= [...]int{3,1,5,10,100} // array initialization 
+	fmt.Println(str, str2, g, f, c, u, pi, n)
+}
 
-	s3:= []int{4,5,9}
-	s4:=make([]int,4)
+func dComplexTypes() {
+	//complex datatypes in Go
+
+	//arrays (fixed size)
+	var a4 [4]int                    // fixed array
+	a5 := [...]int{3, 1, 5, 10, 100} //array initialized with fixed size of five
+
+	//slices (dynamic size)
+	s3 := []int{4, 5, 9} // dynamic sized array dec and init
+	s4 := make([]int, 4) //assigns four sized slice initialized to 0
 	var d2 [][]float64
-	bs:=[]byte("a slice")
+	bs := []byte("a slice")
 
-	s := []int{1, 2, 3}     // Result is a slice of length 3.
-    s = append(s, 4, 5, 6)  // Added 3 elements. Slice now has length of 6.
-	fmt.Println(s)
-	
-	s = append(s, []int{7, 8, 9}...) // Second argument is a slice literal.
-	fmt.Println(s)
-	
+	s3 = append(s3, 4, 5, 6)
+	s3 = append(s3, []int{7, 8, 9}...)
+
+	fmt.Println(s3)
+
 	m := map[string]int{"three": 3, "four": 4}
 	m["one"] = 1
-	
-	//file writing underscore denotes the error variable which is being discarded
-	file, _ := os.Create("output.txt")
-    fmt.Fprint(file, "This is how you write to a file, by the way")
-    file.Close()
 
-    // Unused variables are an error in Go.
-    // The underscore lets you "use" a variable but discard its value.
-	_, _, _, _, _, _, _, _, _, _ = str, s2, g, f, u, pi, n, a5, s4, bs
-	
-	fmt.Println(s, c, a4, s3, d2, m)
+	//unused variables are errors in Go
+	//underscore
+	_, _, _, _, _ = a4, a5, s4, d2, bs
+
+	fmt.Println(a4, a5, s4, d2, bs)
+}
+
+func eWriteToFile() {
+	file, _ := os.Create("output.txt")
+	fmt.Fprint(file, "This is an example to write to file")
+	file.Close()
+}
+
+func fFlowControl() {
+	if 5 > 3 {
+		fmt.Println("True in if clause")
+	} else if 3 > 2 {
+		fmt.Println("If else clause")
+	} else {
+		fmt.Println("Else Clause")
+	}
+
+	x := 42
+	switch x {
+	case 0:
+	case 1:
+	case 42:
+		fmt.Println("Reached 42")
+	case 43:
+		fmt.Println("Unreached")
+	default:
+		fmt.Println("Default case is optional")
+	}
 
 }
